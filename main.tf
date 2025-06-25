57dfb9c29c228e7f1f55dd8a787deba2b1fb44ee
Saichand <32841238+saichandpolasa@users.noreply.github.com>
2025-06-25 17:41:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ module "ec2_instance" {
   ami_value = "ami-05d3e0186c058c4dd" # replace this
   instance_type_value = "t3.small"
 }
+
+resource "aws_instance" "test_instance" {
+  name = "test-instance"
+}
